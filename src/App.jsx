5094f836a818e218a6327c179755de95fcf41548
anopadu2 <f20181240@hyderabad.bits-pilani.ac.in>
2023-04-18 12:07:17 -0400

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { storage } from "./config/firebase";
+import { v4 } from "uuid";
+import { detectLabel } from "./config/vison";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+  const fileInputRef = useRef(null);
+  const imagesListRef = ref(storage, "images/");
+  const MAX_IMAGES = 3;
+
+  const handleChooseFileClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(imagesListRef)
+      .then((response) =>
+        Promise.all(
+          response.items.slice(0, MAX_IMAGES).map((item) => getDownloadURL(item))
+        )
+      )
+      .then((urls) => setImageUrls(urls));
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="upload">
+        <h1>
+          All we need is a picture{" "}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>
+        </h1>
+        <div className="upload-buttons">
+          <input
+            type="file"
+            onChange={(event) => setImageUpload(event.target.files[0])}
+            ref={fileInputRef}
+            style={{ display: "none" }}
+          />
+          <button onClick={handleChooseFileClick}>Choose File</button>
+          <button onClick={uploadFile}>Upload An Image</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <h1> or try some samples...</h1>
+      <div className="image-container">
+        {imageUrls.map((url) => {
+          return <img key={url} src={url} alt="uploaded image" />;
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
